b8924c981cd263bd039e85ec61dcf15b9e6fd93b
Dave Moten <davidmoten@gmail.com>
2016-02-01 10:42:23 +1100

    add classes

---
+++ src/main/java/com/github/davidmoten/rtree3d/ListPair.java
@@ -0,0 +1,44 @@
+package com.github.davidmoten.rtree3d;
+
+import java.util.List;
+
+/**
+ *
+ * Not thread safe.
+ *
+ * @param <T>
+ *            list type
+ */
+public final class ListPair<T extends HasGeometry> {
+    private final Group<T> group1;
+    private final Group<T> group2;
+    // these non-final variable mean that this class is not thread-safe
+    // because access to them is not synchronized
+    private Float areaSum = null;
+    private final Float marginSum;
+
+    public ListPair(List<T> list1, List<T> list2) {
+        this.group1 = new Group<T>(list1);
+        this.group2 = new Group<T>(list2);
+        this.marginSum = group1.geometry().mbr().perimeter() + group2.geometry().mbr().perimeter();
+    }
+
+    public Group<T> group1() {
+        return group1;
+    }
+
+    public Group<T> group2() {
+        return group2;
+    }
+
+    public float areaSum() {
+        if (areaSum == null)
+            areaSum = group1.geometry().mbr().area() + group2.geometry().mbr().area();
+        return areaSum;
+    }
+
+    public float marginSum() {
+        return marginSum;
+    }
+
+}
---
+++ src/main/java/com/github/davidmoten/rtree3d/Util.java
@@ -0,0 +1,23 @@
+package com.github.davidmoten.rtree3d;
+
+import java.util.List;
+
+public class Util {
+
+    public static <T extends HasGeometry> Box mbb(List<T> list) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static List<Entry<T, S>> add(List<Entry<T, S>> entries, Entry<T, S> entry) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static List<? extends Node<T, S>> replace(List<? extends Node<T, S>> children,
+            Node<T, S> child, List<Node<T, S>> list) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
